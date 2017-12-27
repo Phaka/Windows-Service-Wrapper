848bd08c28289c1de2e85b62020e574ec6224e0f
Werner Strydom <werners@bloudraak.com>
2017-12-27 11:44:44 -0800

    Help

---
+++ src/Wrapper/wrapper-command.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "wrapper-error.h"
+#include "service_config.h"
+
+typedef int(*wrapper_command_func)(wrapper_config_t* config, wrapper_error_t** error);
+
+typedef struct wrapper_command_t
+{
+	TCHAR* name;
+	TCHAR* description;
+	wrapper_command_func func;
+} wrapper_command_t;
+
+
+
